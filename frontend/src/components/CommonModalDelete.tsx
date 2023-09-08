9bddbd7ce5bd3ee4bc9cc6388ab196ab79468d42
Константин Яриш <sc-kyarish@t1-integration.ru>
2023-09-09 02:26:11 +0400

    Refactor modals

---
+++ frontend/src/components/CommonModalDelete.tsx
@@ -0,0 +1,57 @@
+import { FC, useState } from 'react';
+import { Button, Icon, Modal } from 'semantic-ui-react';
+
+interface CommonModalDeleteProps {
+  onDelete: () => void;
+  isLoading: boolean;
+  name: string;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+const CommonModalDelete: FC<CommonModalDeleteProps> = ({
+  onDelete,
+  isLoading,
+  name,
+  setOpen,
+  open,
+}) => {
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDelete = () => {
+    onDelete();
+    handleClose();
+  };
+
+  return (
+    <Modal
+      onClose={handleClose}
+      onOpen={handleOpen}
+      open={open}
+      trigger={<Icon link name="trash" />}
+    >
+      <Modal.Header>Удаление записи</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <p>Вы действительно хотите удалить запись "{name}"?</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={handleClose} basic>
+          Отмена
+        </Button>
+        <Button onClick={handleDelete} negative loading={isLoading}>
+          Удалить
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
+
+export default CommonModalDelete;
