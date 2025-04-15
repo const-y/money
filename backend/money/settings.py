bb427a504044cc60370870d9a783ad5ca0768d64
Konstantin Yarish <konstantin.yarish@gmail.com>
2025-04-15 08:59:59 +0300

    Fix database name in Django

--- backend/money/settings.py
+++ backend/money/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'money.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("POSTGRES_NAME"),
+        'NAME': os.getenv("POSTGRES_DB"),
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
