a90d7deaa2185a0e3c04663746dcbfae6346866a
Konstantin Yarish <konstantin.yarish@gmail.com>
2025-04-21 14:31:03 +0300

    Extract UserInfo

--- frontend/src/components/UserMenu.tsx
+++ frontend/src/components/UserMenu.tsx
@@ -1,19 +1,15 @@
-import { Avatar, Group, Menu, Text, UnstyledButton } from '@/components/ui';
+import { Menu, UnstyledButton } from '@/components/ui';
 import routes from '@/constants/routes';
 import { useAuthContext } from '@/context/Auth';
 import useLogoutMutation from '@/hooks/useLogoutMutation';
-import useMeQuery from '@/hooks/useMeQuery';
-import { Box, Loader } from '@mantine/core';
-import { IconChevronDown, IconLogout } from '@tabler/icons-react';
+import { IconLogout } from '@tabler/icons-react';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import UserInfo from './UserInfo';
 
-const useUserMeu = () => {
+const UserMenu: FC = () => {
   const { logout } = useAuthContext();
-
-  const { data: user, isLoading } = useMeQuery();
   const navigate = useNavigate();
-  const avatarSrc = `https://i.pravatar.cc/40?u=${user?.username}}`;
 
   const { mutate: logoutMutate } = useLogoutMutation({
     onSuccess: () => {
@@ -22,39 +18,22 @@ const useUserMeu = () => {
     },
   });
 
-  return { user, isLoading, avatarSrc, logout: logoutMutate };
-};
-
-const UserMenu: FC = () => {
-  const { user, isLoading, avatarSrc, logout } = useUserMeu();
-
-  if (isLoading || !user) {
-    return <Loader size="xs" />;
-  }
+  const handleLogout = () => {
+    logoutMutate();
+  };
 
   return (
     <Menu shadow="md" width={200}>
       <Menu.Target>
         <UnstyledButton>
-          <Group gap="xs">
-            <Avatar src={avatarSrc} alt={user.username} radius="xl" />
-            <Box>
-              <Text size="sm" fw={500} c="white">
-                {user.username}
-              </Text>
-              <Text size="xs" c="dimmed">
-                {user.email}
-              </Text>
-            </Box>
-            <IconChevronDown size={16} />
-          </Group>
+          <UserInfo />
         </UnstyledButton>
       </Menu.Target>
 
       <Menu.Dropdown>
         <Menu.Item
           leftSection={<IconLogout size={16} />}
-          onClick={() => logout()}
+          onClick={handleLogout}
         >
           Выйти
         </Menu.Item>
