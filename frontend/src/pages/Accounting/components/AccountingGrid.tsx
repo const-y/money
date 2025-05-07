90550de66e0b12d09b37e21ab4797da9088eb632
Konstantin Yarish <konstantin.yarish@gmail.com>
2025-05-07 16:43:39 +0300

    Refetch account list

--- frontend/src/pages/Accounting/components/AccountingGrid.tsx
+++ frontend/src/pages/Accounting/components/AccountingGrid.tsx
@@ -1,5 +1,5 @@
+import { Box, Group } from '@/components/ui';
 import { FC } from 'react';
-import { Grid } from 'semantic-ui-react';
 import { useActiveAccount } from '../ActiveAccountContext';
 import AccountingModals from './AccountingModals';
 import AccountsMenu from './AccountsMenu';
@@ -9,16 +9,15 @@ const AccountingGrid: FC = () => {
   const { activeAccountId } = useActiveAccount();
 
   return (
-    <Grid>
-      <Grid.Column width={4}>
+    <Group align="top" h="100%">
+      <Box w="300px">
         <AccountsMenu />
-      </Grid.Column>
-
-      <Grid.Column stretched width={12}>
+      </Box>
+      <Box flex={1}>
         <AccountingModals />
         {activeAccountId && <OperationsTable accountId={activeAccountId} />}
-      </Grid.Column>
-    </Grid>
+      </Box>
+    </Group>
   );
 };
 
